3c7720b8064173f3216e5e1c69594fa7576929c7
Tessmanng9 <tessmanng9@gmail.com>
2025-06-03 12:41:10 -0300

    feat: add BookingItem component

---
+++ src/components/booking-item.tsx
@@ -0,0 +1,37 @@
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
+
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mt-6 text-xs font-bold uppercase text-gray-500">
+        Agendamentos
+      </h2>
+      <Card className="mt-3 bg-muted">
+        <CardContent className="flex justify-between p-0">
+          {/* ESQUERDA */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src="https://github.com/shadcn.png" />
+              </Avatar>
+              <p className="text-sm">Junior</p>
+            </div>
+          </div>
+
+          {/* DIREITA */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Junho</p>
+            <p className="text-2xl">06</p>
+            <p className="text-sm">17:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
