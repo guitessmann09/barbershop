3a1945ad1fa2d850451a0cd98309cf0f99641f60
Gui_Tessmann <tessmanng9@gmail.com>
2025-06-03 21:41:47 -0300

    refactor: remove unused input and search icon, streamline home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import Image from "next/image"
-import { SearchIcon } from "lucide-react"
 import quickSearchOptions from "./_constants/search"
 import BookingItem from "@/components/booking-item"
 import { db } from "@/lib/prisma"
@@ -16,12 +14,19 @@ const Home = async () => {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Guilherme</h2>
         <p className="text-sm text-gray-500">Segunda-feira, 2 de Junho</p>
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Buscar" />
-          <Button>
-            <SearchIcon />
-          </Button>
+
+        {/* BANNER */}
+        <div className="relative mt-5 h-[150px] w-full">
+          <Image
+            src="/dandys-den-banner-01.png"
+            alt="Dandy's Den"
+            fill
+            className="rounded-xl object-cover"
+          />
         </div>
+
+        {/* AGENDAMENTOS */}
+        <BookingItem />
 
         {/* QUICK SEARCH */}
         <div className="my-6 flex items-center gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
@@ -37,19 +42,6 @@ const Home = async () => {
             </Button>
           ))}
         </div>
-
-        {/* BANNER */}
-        <div className="relative h-[150px] w-full">
-          <Image
-            src="/dandys-den-banner-01.png"
-            alt="Dandy's Den"
-            fill
-            className="rounded-xl object-cover"
-          />
-        </div>
-
-        {/* AGENDAMENTOS */}
-        <BookingItem />
 
         {/* SERVICOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-500">
