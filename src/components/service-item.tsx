56e5ef2ce0d1967af9cb999c0029767481506449
Gui_Tessmann <tessmanng9@gmail.com>
2025-06-03 23:07:49 -0300

    feat: add initial booking sheet

--- src/components/service-item.tsx
+++ src/components/service-item.tsx
@@ -1,13 +1,65 @@
+"use client"
+
 import { Service } from "@prisma/client"
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from "./ui/sheet"
+import { Calendar } from "./ui/calendar"
+import { ptBR } from "date-fns/locale"
+import { useState } from "react"
+import { format } from "date-fns"
 
 interface ServiceItemProps {
   service: Service
 }
 
+const TIME_LIST = [
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+]
+
 const ServiceItem = ({ service }: ServiceItemProps) => {
+  const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined)
+  const [selectedTime, setSelectedTime] = useState<string | undefined>(
+    undefined,
+  )
+  const handleSelectDay = (day: Date | undefined) => {
+    setSelectedDay(day)
+  }
+
+  const handleSelectTime = (time: string) => {
+    setSelectedTime(time)
+  }
+
   return (
     <Card>
       <CardContent className="flex items-center gap-3 bg-muted p-3">
@@ -29,9 +81,109 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                 currency: "BRL",
               }).format(Number(service.price))}
             </p>
-            <Button size="sm" variant="outline">
-              Agendar
-            </Button>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button size="sm" variant="outline">
+                  Agendar
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="h-full px-0">
+                <div className="border-b border-solid py-5">
+                  <Calendar
+                    mode="single"
+                    locale={ptBR}
+                    selected={selectedDay}
+                    onSelect={handleSelectDay}
+                    className="rounded-lg"
+                    styles={{
+                      head_cell: {
+                        width: "100%",
+                        textTransform: "capitalize",
+                      },
+                      cell: {
+                        width: "100%",
+                      },
+                      button: {
+                        width: "100%",
+                      },
+                      nav_button_previous: {
+                        width: "32px",
+                        height: "32px",
+                      },
+                      nav_button_next: {
+                        width: "32px",
+                        height: "32px",
+                      },
+                      caption: {
+                        textTransform: "capitalize",
+                      },
+                    }}
+                  />
+                </div>
+
+                {selectedDay && (
+                  <div className="flex gap-3 overflow-x-scroll border-b border-solid p-5 [&::-webkit-scrollbar]:hidden">
+                    {TIME_LIST.map((time) => (
+                      <Button
+                        key={time}
+                        variant={selectedTime === time ? "default" : "outline"}
+                        className="w-full rounded-full border"
+                        onClick={() => handleSelectTime(time)}
+                      >
+                        {time}
+                      </Button>
+                    ))}
+                  </div>
+                )}
+
+                {/* TODO: Adicionar seleção de profissional */}
+
+                {selectedTime && selectedDay && (
+                  <div className="p-5">
+                    <Card>
+                      <CardContent className="space-y-3 p-4">
+                        <div className="flex items-center justify-between">
+                          <h2 className="text-sm font-bold">{service.name}</h2>
+                          <p className="text-sm font-bold">
+                            {Intl.NumberFormat("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            }).format(Number(service.price))}
+                          </p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <h2 className="text-sm text-muted-foreground">
+                            Data
+                          </h2>
+                          <p className="text-sm">
+                            {format(selectedDay, "d 'de' MMMM", {
+                              locale: ptBR,
+                            })}
+                          </p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <h2 className="text-sm text-muted-foreground">
+                            Horário
+                          </h2>
+                          <p className="text-sm">{selectedTime}</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <h2 className="text-sm text-muted-foreground">
+                            Profissional
+                          </h2>
+                          <p className="text-sm">Juninho</p>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                )}
+                <SheetFooter className="px-5">
+                  <SheetClose asChild>
+                    <Button>Confirmar</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </CardContent>
