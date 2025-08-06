f9eda729e671afd35e803c4815f1906b76892cbe
Gui_Tessmann <tessmanng9@gmail.com>
2025-08-06 14:28:58 -0300

    feat: add redirect to correct home page

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { Toaster } from "sonner"
+import Footer from "@/components/footer"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+  title: "Dandy's Den",
+  description: "Barbearia Dandy's Den",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} flex h-screen flex-col`}>
+        {children}
+      </body>
+    </html>
+  )
+}
